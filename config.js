b7774bcea2ca495709d8785e07c043ef612fda58
sonal2694 <nikki2694@gmail.com>
2018-07-07 14:12:48 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion' 
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '265223194253232',
+        clientSecret: '7c3d68994fa55598a4b02e4a43806d0c'
+    }
 }
